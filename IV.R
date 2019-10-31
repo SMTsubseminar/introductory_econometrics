8ae87bdc7c662cd6d13b68109715f4ff1df596cb
Ryuya Ko <ryuyakyo25@gmail.com>
2019-10-31 23:47:14 +0900

    Add: implementation by hand of IV est.

--- IV.R
+++ IV.R
@@ -10,14 +10,18 @@ library(AER);library(stargazer);library(wooldridge);library(dplyr)
 data(card);head(card);help(card)
 summary(card)
 
-card <- as.data.frame(card)
 
-head(card)
-
-
+###### cleansing/preprocessing
 # apply(is.na(card), MARGIN=2, sum)
 # card.cld <- na.omit(card)
 # apply(is.na(card.cld), MARGIN=2, sum)
+card.cld <- card.cld %>%
+  mutate()
+
+card %>%
+  mutate(reg666=as.factor(reg666)) %>%
+  summary()
+
 
 ### IV by package
 
@@ -85,6 +89,79 @@ stargazer(rel.check, ols, reg.iv, type='latex', keep=c("educ", "nearc4", "age", 
 
 #### Implementation by hand!!
 
+proj_P <- function(X, Z, Y){
+  projecter <- solve(t(Z) %*% X) %*% t(Z)
+  return(projecter %*% Y)
+}
 
 
 
+est_beta <- function(X, y){
+  bhat <- proj_P(Z, Z, y)
+  return(bhat)
+}
+
+
+fitting <- function(y, X, bhat){
+  fitted <- X %*% bhat
+  resid <- y - fitted
+  return(list(fitted=fitted, resid=resid))
+}
+
+card.blck <- card %>%
+  filter(black==1) %>%
+  as.data.frame()
+
+summary(card.blck)
+
+X <- card.blck[c('educ', 'exper', 'expersq', 'reg662', 'reg663', 'reg664',
+                 'reg665', 'reg666', 'reg667', 'reg668', 'reg669')] %>%
+  as.matrix()
+Z <- card.blck[c('nearc4', 'exper', 'expersq', 'reg662', 'reg663', 'reg664',
+                 'reg665', 'reg666', 'reg667', 'reg668', 'reg669')] %>%
+  as.matrix()
+
+# just identified
+
+y <- card.blck[, 'lwage'] %>%
+  as.matrix()
+n <- length(y)
+
+iv_est <- function(Z, X, y){
+  n <- length(y)
+  
+  b.iv <- proj_P(Z, X, y)
+  fit.res <- fitting(y, X, b.iv)
+  
+  QZ <- t(Z) %*% Z
+  QX <- t(Z) %*% X
+  sigma.hat <- as.numeric(t(fit.res$resid) %*% fit.res$resid / n)
+  est.cov <- sigma.hat * solve(QX) %*% QZ %*% t(solve(t(X)%*%Z))
+  return(list(b.iv=b.iv, fitted=fit.res$fitted, resid=fit.res$resid, est.cov=est.cov, sigma.hat2=sigma.hat))
+}
+
+res <- iv_est(Z, X, y)
+res$b.iv
+head(res$resid)
+se <- diag(res$est.cov)
+print(se)
+
+tstat <- res$b.iv[1]/se[1] # not significant
+
+# 95%CI
+c(res$b.iv[1] -1.96 * se[1], res$b.iv[1] + 1.96 * se[1])
+
+
+
+
+### Two stage least squares
+# do_tsls <- function(X, Z, y){
+#   X.hat <-Z %*% proj_P(Z, X)
+#   resid.X <- X - X.hat
+#   b.iv <- proj_P(Z, y)
+#   resid.y <- y - X.hat %*% b.iv
+#   return(list(Xhat=X.hat, resid1=resid.X, b.iv=b.iv, resid2=resid.y))
+# }
+# res <- do_tsls(X, Z, y)
+# 
+# res$b.iv
